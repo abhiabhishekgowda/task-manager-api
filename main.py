a1821cce5169efcbf782dc527493c9e222207844
Abhishek.k <ceoabhi.k@gmail.com>
2026-06-18 22:20:01 +0530

    Day2: "Learned GET routes and path parameters"

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
+task_db = [
+    {"id": 1, "title": "study fastapi"},
+    {"id": 2, "title": "solve leetcode"}
+]
 
 @app.get("/")
 def home():
-    return {"message": "Task Manager API is running!"}
+    return {"message": "welcom to your task manager"}
+
+@app.get("/tasks")
+def view_all_tasks():
+    return {"tasks": task_db}
+
+@app.get("/task/{id}")
+def view_task(id: int):
+    for task in task_db:
+        if task["id"] == id:
+            return {"task": task}
+    return {"message": "task not found"}
+
+@app.post("/task")
+def create_task(id: int, title: str):
+    task_db.append({"id": id, "title": title})
+    return {"message": "task created successfully"}
+    
+
+
+
