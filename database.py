08cca4f404ca04320e6b46f6a3f092c26ceb30be
Abhishek.k <ceoabhi.k@gmail.com>
2026-07-03 19:39:23 +0530

    DAY2: "feat: refactor database initialization into an OOP TaskDatabase class"

--- database.py
+++ database.py
@@ -2,20 +2,23 @@ import sqlite3
 
 DB_NAME = "tasks.db"
 
-def init_db():
-    with sqlite3.connect(DB_NAME) as conn:
-        cursor = conn.cursor()
-        
-        # Create our single table with proper SQLite types
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS tasks (
-            id INTEGER PRIMARY KEY,
-            title TEXT NOT NULL,
-            completed BOOLEAN NOT NULL
-        )
-        """)
-        
-        conn.commit()
-    print("📢 Database initialized and tasks table is ready!")
+class TaskDatabase:
+    # 1. This function sets up the properties
+    def __init__(self, db_name="tasks.db"):
+        self.db_name = db_name
+        self.create_tables()  # 📢 This calls the method below!
 
-init_db ()
+    # 2. FIX: Shift this back to the left! It belongs to the Class, not __init__
+    def create_tables(self):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+            CREATE TABLE IF NOT EXISTS tasks (
+                id INTEGER PRIMARY KEY,
+                title TEXT NOT NULL,
+                completed BOOLEAN NOT NULL
+            )
+            """)
+            conn.commit()
+        print("📢 Database initialized and tasks table is ready!")
+
