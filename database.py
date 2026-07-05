b84b7d88b5cb20f41d90abfd2ce0f90c55b910e6
Abhishek.k <ceoabhi.k@gmail.com>
2026-07-05 19:35:10 +0530

    WEEK2: DAY 3:" Initialize SQLite database and tasks table structure"

--- database.py
+++ database.py
@@ -1,24 +1,19 @@
 import sqlite3
 
-DB_NAME = "tasks.db"
-
 class TaskDatabase:
-    # 1. This function sets up the properties
-    def __init__(self, db_name="tasks.db"):
+    def __init__(self, db_name = "tasks.db"):
         self.db_name = db_name
-        self.create_tables()  # 📢 This calls the method below!
-
-    # 2. FIX: Shift this back to the left! It belongs to the Class, not __init__
-    def create_tables(self):
+        self.create_table()
+    def create_table(self):
         with sqlite3.connect(self.db_name) as conn:
             cursor = conn.cursor()
             cursor.execute("""
-            CREATE TABLE IF NOT EXISTS tasks (
+            CREATE TABLE IF NOT EXISTS tasks(
                 id INTEGER PRIMARY KEY,
                 title TEXT NOT NULL,
-                completed BOOLEAN NOT NULL
+                completed BOOLEAN NOT NULL DEFAULT 0
             )
             """)
             conn.commit()
-        print("📢 Database initialized and tasks table is ready!")
+            print("Database successfully.")
 
